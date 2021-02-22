a25b5bf34c5921da10895a48535ea029ef123685
Stephen Kuforiji <stephenkuforiji@crenettechlabs.com>
2021-02-22 10:04:40 +0100

    first code chunks

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+	purge: [],
+	darkMode: false, // or 'media' or 'class'
+	important: true,
+	theme: {
+		extend: {
+			colors: {
+				"primary-brand": "#8139E6",
+				"primary-brand-light": "#E5DCF1",
+				"primary-dark": "#222222",
+				"primary-dark-light": "rgba(34, 34, 34, 0.6)",
+				"primary-gray": "#e5e5e5",
+				"primary-brand-gray": "#D3D3D3",
+				"input-gray": "#131516",
+				"secondary-dark-light": " rgba(34, 34, 34, 0.8)",
+				"secondary-dark-lighter": "rgba(34, 34, 34, 0.25)",
+				"fb-blue": "#385C8E",
+				"tw-blue": "#03A9F4",
+			},
+			width: {
+				"37p": "37%",
+				"85p": "85%",
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
